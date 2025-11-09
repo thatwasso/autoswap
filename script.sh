59ab63f156af25c7866e643c0bb6530a91132bc0
thatwasso <46354493+thatwasso@users.noreply.github.com>
2025-11-09 09:44:05 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+(
+echo e #resize partitions
+echo #accept default:3
+echo +6G #root partition 6GB
+echo n #create partition
+echo #accept default:4
+echo #first sector
+echo +2G #partition size 2GB
+echo t #partition type
+echo #accept default:4
+echo Linux swap #partition type
+echo w #write
+) | sudo fdisk
+
+sudo mkswap /dev/sda4
+swapon /dev/sda4
